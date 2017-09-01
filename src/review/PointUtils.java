52995dc55ef2c89f16719c9078de7ec0e80d6402
Maja Frydrychowicz <mjzffr@gmail.com>
2017-08-31 21:12:53 -0400

    Add example of sorting an array

---
+++ src/review/PointUtils.java
@@ -0,0 +1,30 @@
+package review;
+
+public class PointUtils {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void sort(Point[] array) {
+		int smallestIndex;
+		for (int i = 0; i < array.length - 1; i++) {
+			// find smallest index
+			smallestIndex = i;
+			for (int j = i + 1; j < array.length; j++) {
+				if (array[j].getX() < array[smallestIndex].getX())
+					smallestIndex = j;
+			}
+
+			// swap values
+			if (i != smallestIndex) {
+				Point temp = array[i];
+				array[i] = array[smallestIndex];
+				array[smallestIndex] = temp;
+			}
+		}
+	}
+	
+
+}
