e1616fc58e5d4a95789be58b1521bae4fd3784da
Maja Frydrychowicz <mjzffr@gmail.com>
2017-08-31 21:20:36 -0400

    Fill in main method in PointUtils
    It shows an example of what an array of Point looks
    like before and after sorting.

--- src/geometry/PointUtils.java
+++ src/geometry/PointUtils.java
@@ -3,8 +3,18 @@ package geometry;
 public class PointUtils {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Point[] dots = new Point[3];
+		dots[0] = new Point(1,2);
+		dots[1] = new Point(4,1);
+		dots[2] = new Point(0,5);
+		for (int i = 0; i < dots.length; i++) {
+			System.out.println(dots[i]);
+		}
+		sort(dots);
+		System.out.println("After srting");
+		for (int i = 0; i < dots.length; i++) {
+			System.out.println(dots[i]);
+		}		
 	}
 
 	public static void sort(Point[] array) {
